667fa0bebbcd96f433dece02a202f067c214cef6
inBRAINS <36745936+inbrains1@users.noreply.github.com>
2018-02-26 23:41:33 +0200

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,118 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+(function() {
+	var cdSlider = document.querySelector('.cd-slider'),
+		item = cdSlider.querySelectorAll("li"),
+		nav = cdSlider.querySelector("nav");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+	item[0].className = "current_slide";
 
-        console.log('Received Event: ' + id);
-    }
-};
+	for (var i = 0, len = item.length; i < len; i++) {
+		var color = item[i].getAttribute("data-color");
+		item[i].style.backgroundColor=color;
+	}
+
+	// Detect IE
+	// hide ripple effect on IE9
+	var ua = window.navigator.userAgent;
+		var msie = ua.indexOf("MSIE");
+		if ( msie > 0 ) {
+			var version = parseInt(ua.substring(msie+ 5, ua.indexOf(".", msie)));
+			if (version === 9) { cdSlider.className = "cd-slider ie9";}
+	}
+
+	if (item.length <= 1) {
+		nav.style.display = "none";
+	}
+
+	function prevSlide() {
+		var currentSlide = cdSlider.querySelector("li.current_slide"),
+			prevElement = currentSlide.previousElementSibling,
+			prevSlide = ( prevElement !== null) ? prevElement : item[item.length-1],
+			prevColor = prevSlide.getAttribute("data-color"),
+			el = document.createElement('span');
+
+		currentSlide.className = "";
+		prevSlide.className = "current_slide";
+
+		nav.children[0].appendChild(el);
+
+		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+		    ripple = nav.children[0].querySelector("span");
+
+		ripple.style.height = size + 'px';
+		ripple.style.width = size + 'px';
+		ripple.style.backgroundColor = prevColor;
+
+		ripple.addEventListener("webkitTransitionEnd", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+		ripple.addEventListener("transitionend", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+	}
+
+	function nextSlide() {
+		var currentSlide = cdSlider.querySelector("li.current_slide"),
+			nextElement = currentSlide.nextElementSibling,
+			nextSlide = ( nextElement !== null ) ? nextElement : item[0],
+			nextColor = nextSlide.getAttribute("data-color"),
+			el = document.createElement('span');
+
+		currentSlide.className = "";
+		nextSlide.className = "current_slide";
+
+		nav.children[1].appendChild(el);
+
+		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+			  ripple = nav.children[1].querySelector("span");
+
+		ripple.style.height = size + 'px';
+		ripple.style.width = size + 'px';
+		ripple.style.backgroundColor = nextColor;
+
+		ripple.addEventListener("webkitTransitionEnd", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+		ripple.addEventListener("transitionend", function() {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		});
+
+	}
+
+	updateNavColor();
+
+	function updateNavColor () {
+		var currentSlide = cdSlider.querySelector("li.current_slide");
+
+		var nextColor = ( currentSlide.nextElementSibling !== null ) ? currentSlide.nextElementSibling.getAttribute("data-color") : item[0].getAttribute("data-color");
+		var	prevColor = ( currentSlide.previousElementSibling !== null ) ? currentSlide.previousElementSibling.getAttribute("data-color") : item[item.length-1].getAttribute("data-color");
+
+		if (item.length > 2) {
+			nav.querySelector(".prev").style.backgroundColor = prevColor;
+			nav.querySelector(".next").style.backgroundColor = nextColor;
+		}
+	}
+
+	nav.querySelector(".next").addEventListener('click', function(event) {
+		event.preventDefault();
+		nextSlide();
+		updateNavColor();
+	});
+
+	nav.querySelector(".prev").addEventListener("click", function(event) {
+		event.preventDefault();
+		prevSlide();
+		updateNavColor();
+	});
+
+})();
